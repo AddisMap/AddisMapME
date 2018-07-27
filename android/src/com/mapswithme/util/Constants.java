bca719531e04ef9e4ebad766e977843c26c1c6f4
Alexander Menk <a.menk@imi.de>
2018-07-27 23:35:49 +0200

    Addismap Branding URLs: maps.me -> me.addismap.com
    (cherry picked from commit f6089c7)

--- android/src/com/mapswithme/util/Constants.java
+++ android/src/com/mapswithme/util/Constants.java
@@ -24,12 +24,13 @@ public final class Constants
 
     public static final String PLAY_MARKET_HTTPS_APP_PREFIX = "https://play.google.com/store/apps/details?id=";
 
-    public static final String FB_MAPSME_COMMUNITY_HTTP = "http://www.facebook.com/MapsWithMe";
+    public static final String FB_MAPSME_COMMUNITY_HTTP = "http://www.facebook.com/AddisMap";
     // Profile id is taken from http://graph.facebook.com/MapsWithMe
-    public static final String FB_MAPSME_COMMUNITY_NATIVE = "fb://profile/111923085594432";
-    public static final String TWITTER_MAPSME_HTTP = "https://twitter.com/MAPS_ME";
+    public static final String FB_MAPSME_COMMUNITY_NATIVE = "fb://profile/AddisMap";
+    public static final String TWITTER_MAPSME_HTTP = "https://twitter.com/AddisMap";
 
-    public static final String WEB_SITE = "http://maps.me";
+    public static final String WEB_SITE = "http://me.addismap.com";
+    public static final String WEB_BLOG = "http://blog.me.addismap.com";
 
     public static final String COPYRIGHT = "file:///android_asset/copyright.html";
     public static final String FAQ = "file:///android_asset/faq.html";
@@ -44,9 +45,10 @@ public final class Constants
 
   public static class Email
   {
-    public static final String FEEDBACK = "android@maps.me";
+    public static final String FEEDBACK = "android@me.addismap.com";
     public static final String SUPPORT = BuildConfig.SUPPORT_MAIL;
-    public static final String RATING = "rating@maps.me";
+    public static final String SUBSCRIBE = "subscribe@me.addismap.com";
+    public static final String RATING = "rating@me.addismap.com";
 
     private Email() {}
   }
@@ -54,9 +56,9 @@ public final class Constants
   public static class Package
   {
     public static final String FB_PACKAGE = "com.facebook.katana";
-    public static final String MWM_PRO_PACKAGE = "com.mapswithme.maps.pro";
+    public static final String MWM_PRO_PACKAGE = "com.addismap.me";
     public static final String MWM_LITE_PACKAGE = "com.mapswithme.maps";
-    public static final String MWM_SAMSUNG_PACKAGE = "com.mapswithme.maps.samsung";
+    public static final String MWM_SAMSUNG_PACKAGE = "com.addismap.me.samsung";
     public static final String TWITTER_PACKAGE = "com.twitter.android";
 
     private Package() {}
